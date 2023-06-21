99381e75e470a3a3ca11d99744626fc287d1cff8
Maria Rios Navarro <propscode254@gmail.com>
2023-06-21 09:57:21 +0200

    npm i swr and use SWRConfig in _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,27 @@
 import GlobalStyle from "../styles";
 import Head from "next/head";
+import { SWRConfig } from "swr";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
-      <Head>
-        <title>Capstone Project</title>
-      </Head>
-      <Component {...pageProps} />
+      <SWRConfig
+        value={{
+          fetcher: async (...args) => {
+            const response = await fetch(...args);
+            if (!response.ok) {
+              throw new Error(`Request with ${JSON.stringify(args)} failed.`);
+            }
+            return await response.json();
+          },
+        }}
+      >
+        <GlobalStyle />
+        <Head>
+          <title>Capstone Project</title>
+        </Head>
+        <Component {...pageProps} />
+      </SWRConfig>
     </>
   );
 }
