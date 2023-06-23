8d346a1a1ffbdb77cbb06bd7a8e636b37357bffb
Maria Rios Navarro <propscode254@gmail.com>
2023-06-23 11:12:25 +0200

    Add the first Icon/SimulationAPP to Home

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,34 @@
 import styled from "styled-components";
 import Link from "next/link";
 import Navigation from "../components/Navigation";
+import StyledAppContainer from "../components/StyledAppContainer";
+import { calenderSVG } from "../components/assets/homeSVG";
 
 const StyledMain = styled.main``;
 
+const StyledAppHeading = styled.h5`
+  font-size: 10px;
+  color: black;
+`;
+
+const StyledWrapAppLink = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  align-items: 48px;
+  justify-self: center;
+  align-self: center;
+  text-decoration: none;
+`;
 //----------------------------------------------- FUNCTION------------HERE
 
 export default function Home() {
   return (
     <>
       <StyledMain>
+        <StyledWrapAppLink href={"/"}>
+          <StyledAppContainer StyledSvg={calenderSVG} />
+          <StyledAppHeading>Kalender</StyledAppHeading>
+        </StyledWrapAppLink>
         <Link href={"/contacts"}>Kontakte</Link>
       </StyledMain>
       <Navigation selected={"home"} />
