f5003b926f642060dc15dc6d6f3a6225696fc538
Maria Rios Navarro <propscode254@gmail.com>
2023-06-23 09:56:21 +0200

    Transfer the contacts to your page and create the Home (Simulation)

--- pages/index.js
+++ pages/index.js
@@ -1,71 +1,12 @@
-import Heading from "../components/Heading";
-import ContactListItem from "../components/ContactListItem";
 import styled from "styled-components";
-import { StyledHR } from "../components/StyledHR";
-import { addSVG } from "../components/assets/contactsSVG";
 import Link from "next/link";
-import useSWR from "swr";
-
-// -----------------------------------STYLE
-const StyledList = styled.ul`
-  list-style-type: style none;
-  display: flex;
-  flex-direction: column;
-  padding-left: 0;
-`;
-
-const StyledHeader = styled.header`
-  display: flex;
-  justify-content: space-between;
-`;
-
-const StyledAddSVGContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 50%;
-  width: 34px;
-  height: 34px;
-  margin: 1.5rem;
-`;
 
 //----------------------------------------------- FUNCTION------------HERE
 
-let href = "/create";
-
 export default function Home() {
-  const { data, isLoading } = useSWR("/api/contacts");
-
-  if (isLoading) {
-    return <h1>Loading...</h1>;
-  }
-
-  if (!data) {
-    return <h1>Keine Daten Gefunden</h1>;
-  }
-
   return (
     <main>
-      <StyledHeader>
-        <Heading>Kontakte</Heading>
-        <Link href={href}>
-          <StyledAddSVGContainer>{addSVG}</StyledAddSVGContainer>
-        </Link>
-      </StyledHeader>
-
-      <StyledList>
-        <StyledHR />
-        {/* Map the contacts to render for one Contact, one Contactlistitem  */}
-        {data.map((contact) => {
-          return (
-            <ContactListItem
-              key={contact._id}
-              id={contact._id}
-              name={contact.name}
-            />
-          );
-        })}
-      </StyledList>
+      <Link href={"/contacts"}>Kontakte</Link>
     </main>
   );
 }
