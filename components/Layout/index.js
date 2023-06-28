746ea695964f2b4090c2db82231ab767c5cd1561
Maria Rios Navarro <propscode254@gmail.com>
2023-06-28 16:40:11 +0200

    create Layout to use it in _app

---
+++ components/Layout/index.js
@@ -0,0 +1,41 @@
+import { useContext } from "react";
+import styled from "styled-components";
+import { ThemeContext } from "styled-components";
+
+//This will be the general container that will give the style to the app,
+// and through it we will be able to change its appearance.
+// I'm going to take this container to the _app,
+// so that each page rendered will have these styles
+
+const StyledAplicationContainer = styled.div`
+  height: 100vh;
+  /* which will dynamically change the background according to the chosen thema. */
+  background-color: ${(props) => {
+    if (props.theme === "light") {
+      return "#ffeacc";
+    } else if (props.theme === "dark") {
+      return "#192559";
+    } else {
+      return "transparent";
+    }
+  }};
+  /* which will dynamically change the color according to the chosen thema. */
+  color: ${(props) => {
+    if (props.theme === "light") {
+      return "black";
+    } else if (props.theme === "dark") {
+      return "#ffeacc";
+    } else {
+      return "black";
+    }
+  }};
+`;
+
+export default function Layout({ children }) {
+  const { theme } = useContext(ThemeContext);
+  return (
+    <StyledAplicationContainer theme={theme}>
+      {children}
+    </StyledAplicationContainer>
+  );
+}
