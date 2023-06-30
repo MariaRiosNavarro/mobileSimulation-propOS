43c823867e7a17503716f2f221d6cb702fb172d3
Maria Rios Navarro <propscode254@gmail.com>
2023-06-30 09:53:45 +0200

    done detail message page without keyboard

--- pages/contacts-message/[id]/index.js
+++ pages/contacts-message/[id]/index.js
@@ -1,16 +1,44 @@
-import Link from "next/link";
 import useSWR from "swr";
 import styled from "styled-components";
+import { backSVG } from "../../../components/assets/contactsSVG";
 import {
   StyledLoading,
   StyledAppBodyContainer,
 } from "../../../components/components.style";
 import AppsHeader from "../../../components/Appsheader";
 import { useRouter } from "next/router";
+import Link from "next/link";
+import { StyledImagePlaceholder } from "../../../components/components.style";
+
+const StyledAppHeaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledContactHeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column-reverse;
+  justify-content: center;
+  align-items: center;
+`;
 
 const StyledHeader = styled.h5`
   font-size: small;
   font-weight: bold;
+`;
+const StyledLink = styled(Link)`
+  margin: 0;
+  padding: 0;
+`;
+
+const StyledKeyboardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 1rem;
+  border: 1px solid black;
+  /* position: relative;
+  bottom: 2rem; */
 `;
 
 //----------------------------------------------- FUNCTION------------HERE
@@ -33,8 +61,15 @@ export default function MessageDetail() {
   return (
     <>
       <StyledAppBodyContainer>
-        <AppsHeader>Nachrichten</AppsHeader>
-        <StyledHeader>{contact.name}</StyledHeader>
+        <StyledAppHeaderContainer>
+          <StyledLink href={"/contacts"}>{backSVG}</StyledLink>
+          <AppsHeader heading="Nachrichten" />
+        </StyledAppHeaderContainer>
+        <StyledContactHeaderContainer>
+          <StyledHeader>{contact.name}</StyledHeader>
+          <StyledImagePlaceholder />
+        </StyledContactHeaderContainer>
+        <StyledKeyboardContainer></StyledKeyboardContainer>
       </StyledAppBodyContainer>
     </>
   );
