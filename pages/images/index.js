83bdc721070bb9ece551d4632a7197ba2008002b
Maria Rios Navarro <propscode254@gmail.com>
2023-07-04 09:41:58 +0200

    repair Typo

--- pages/images/index.js
+++ pages/images/index.js
@@ -1,11 +1,11 @@
-import ApppHeader from "../../components/AppsHeader";
+import AppsHeader from "../../components/AppsHeader";
 import { cameraSmallSVG } from "../../components/assets/homeSVG";
 
 import PhotoList from "../../components/PhotoList";
 export default function Gallery() {
   return (
     <>
-      <ApppHeader heading={"Gallerie"} StyledSvg={cameraSmallSVG} />
+      <AppsHeader heading={"Gallerie"} StyledSvg={cameraSmallSVG} />
       <PhotoList></PhotoList>
     </>
   );
