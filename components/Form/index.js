121ab6d683e0de235e97dfeb070a4015ca11658f
Maria Rios Navarro <propscode254@gmail.com>
2023-07-05 12:20:46 +0200

    Add in Form the image handle function, and change on Submit function in create. Dont work

--- components/Form/index.js
+++ components/Form/index.js
@@ -8,12 +8,14 @@ import { StyledButtonsContainer } from "../components.style";
 import { ThemeContext } from "../../pages/_app";
 import { useContext } from "react";
 import { StyledInputName } from "../Layout";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 //------------------------------------------STYLE
 
 //DO the ugly upload files input invisible
 const StyledInputPhoto = styled.input`
-  display: none;
+  /* display: none; */
 `;
 
 //Style for the input Photo label to replace the ugly uplaud file
@@ -22,11 +24,11 @@ const StyledPhotoLabel = styled.label`
   display: flex;
   align-items: center;
   justify-content: center;
-  border-radius: 50%;
+  /* border-radius: 50%; */
   background-color: lightgray;
   border: 2px solid var(--primary-color);
-  width: 34px;
-  height: 34px;
+  /* width: 34px;
+  height: 34px; */
   outline: none;
   &:hover {
     border: 2px solid var(--hover-color);
@@ -55,11 +57,30 @@ const StyledTextareaGray = styled.textarea`
   }
 `;
 
-export default function Form({ onSubmit, formName, defaultData, onClick }) {
+export default function Form({ onSubmit, defaultData, onClick }) {
   const { theme, customColor } = useContext(ThemeContext);
+  // const [photo, setPhoto] = useState("");
+  const router = useRouter();
+
+  let photoUrl = "";
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const ContactFormData = Object.fromEntries(formData);
+    const response = await fetch("/api/image/upload", {
+      method: "POST",
+      body: formData,
+    });
+    const photoDetails = await response.json();
+    photoUrl = photoDetails.url;
+    ContactFormData.photo = photoUrl;
+    onSubmit(ContactFormData);
+  }
+
   return (
     <>
-      <form onSubmit={onSubmit} aria-labelledby={formName}>
+      <form onSubmit={handleSubmit}>
         <StyledHeadingandFoto>
           {/* NAME INPUT */}
           <label htmlFor="name">
@@ -81,6 +102,7 @@ export default function Form({ onSubmit, formName, defaultData, onClick }) {
               id="photo"
               name="photo"
               defaultValue={defaultData?.photo}
+              // onChange={setPhoto}
             />
           </StyledPhotoLabel>
         </StyledHeadingandFoto>
