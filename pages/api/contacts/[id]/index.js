91061a585353f5bd84170d3a9fe70de194f02828
Maria Rios Navarro <propscode254@gmail.com>
2023-07-05 20:13:50 +0200

    neu api in [id]/index.js everything works but favorites not.

--- pages/api/contacts/[id]/index.js
+++ pages/api/contacts/[id]/index.js
@@ -1,34 +1,63 @@
 import dbConnect from "../../../../db/connect";
 import Contact from "../../../../db/models/contact";
+// export default async function handler(request, response) {
+//   await dbConnect();
+//   const { id } = request.query;
+//   if (request.method === "GET") {
+//     const contact = await Contact.findById(id);
+//     if (!contact) {
+//       return response.status(404).json({ status: "Not Found" });
+//     }
+//     response.status(200).json(contact);
+//   }
+//   //API for UPDATE
+//   if (request.method === "PUT") {
+//     try {
+//       const { favorite } = request.body;
+//       const contact = await Contact.findById(id);
+//       if (!contact) {
+//         return response.status(404).json({ error: "Contact not found" });
+//       }
+//       contact.favorite = favorite;
+//       await contact.save();
+//       return response.status(200).json({ message: "Contact updated" });
+//     } catch (error) {
+//       console.log(error);
+//       return response.status(500).json({ error: "Server error" });
+//     }
+//   }
+//   if (request.method === "DELETE") {
+//     await Contact.findByIdAndDelete(id);
+//     response.status(200).json({ status: "Contact deleted!" });
+//     return;
+//   }
+// }
+
 export default async function handler(request, response) {
   await dbConnect();
   const { id } = request.query;
+
+  if (!id) {
+    return;
+  }
   if (request.method === "GET") {
     const contact = await Contact.findById(id);
+
     if (!contact) {
-      return response.status(404).json({ status: "Not Found" });
+      return response.status(404).json({ status: "Not found" });
     }
+
     response.status(200).json(contact);
-  }
-  //API for UPDATE
-  if (request.method === "PUT") {
-    try {
-      const { favorite } = request.body;
-      const contact = await Contact.findById(id);
-      if (!contact) {
-        return response.status(404).json({ error: "Contact not found" });
-      }
-      contact.favorite = favorite;
-      await contact.save();
-      return response.status(200).json({ message: "Contact updated" });
-    } catch (error) {
-      console.log(error);
-      return response.status(500).json({ error: "Server error" });
-    }
-  }
-  if (request.method === "DELETE") {
-    await Contact.findByIdAndDelete(id);
-    response.status(200).json({ status: "Contact deleted!" });
     return;
+  }
+
+  if (request.method === "PATCH") {
+    await Contact.findByIdAndUpdate(id, { $set: request.body });
+    response.status(200).json({ status: "Contact updated" });
+  }
+
+  if (request.method === "DELETE") {
+    await Place.findByIdAndDelete(id);
+    response.status(200).json({ status: "Contact deleted!" });
   }
 }
