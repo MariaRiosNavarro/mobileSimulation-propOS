8baa6ec353c5bafac810cfaded726f49145af4c7
Maria Rios Navarro <propscode254@gmail.com>
2023-07-06 15:21:53 +0200

    change content

--- pages/api/ai/index.js
+++ pages/api/ai/index.js
@@ -10,7 +10,7 @@ export default async function handler(request, response) {
       messages: [
         {
           role: "user",
-          content: `create a simple ${answer}  in german language and no longer than 200 words to the user's ${question}.Display the Answer as a json object `,
+          content: `create a simple ${answer}  in german language and no longer than 200 words to the user's ${question}. If you can't answer the question, tell the user to ask a simple question, but always give an answer.Display the Answer as a json object. `,
         },
       ],
     });
