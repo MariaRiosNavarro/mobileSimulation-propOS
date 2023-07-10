616290a25bec229cff9fb0b16fe99c6780f3fdb9
Maria Rios Navarro <propscode254@gmail.com>
2023-07-10 08:39:46 +0200

    Add the buttonTheme to Keyboard to change each of the buttons

--- components/MyKeyboard/index.js
+++ components/MyKeyboard/index.js
@@ -148,6 +148,13 @@ export default function MyKeyboard() {
         onChange={onChange}
         onKeyPress={onKeyPress}
         layout={costumlayout}
+        buttonTheme={[
+          {
+            class: "hg-dark",
+            buttons:
+              "🚀 🐬 🐢 🥳 ❤️ 🤣 👩🏽‍💻 🌹 🍺 😭 😟 1 2 3 4 5 6 7 8 9 0 \u00DF q w e r t z u i o p \u00FC a s d f g h j k l \u00F6 \u00E4 {enter} {shift} y x c v b n m {bksp} .com @ {space} \u00B0 ! \" \u00A7 $ % & / ( ) = ? ` \' {tab} Q W E R T Z U I O P \u00DC \* {lock} A S D F G H J K L \u00D6 \u00C4 '  {shift} > Y X C V B N M ; : _ {shift} .com @ {space}",
+          },
+        ]}
       />
     </StyledContainer>
   );
