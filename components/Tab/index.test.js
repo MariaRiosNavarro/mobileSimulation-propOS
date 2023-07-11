65503d227cbd00d92e2fdd2066dc1937849041f0
Maria Rios Navarro <propscode254@gmail.com>
2023-07-11 17:29:55 +0200

    Add test to Tab

---
+++ components/Tab/index.test.js
@@ -0,0 +1,18 @@
+import { render, fireEvent } from "@testing-library/react";
+import Tab from "../Tab";
+
+describe("Tab Component", () => {
+  test("renders tab with provided children", () => {
+    const { getByText } = render(<Tab>Favoriten</Tab>);
+    const tabElement = getByText("Favoriten");
+    expect(tabElement).toBeInTheDocument();
+  });
+
+  test("fires onClick event when tab is clicked", () => {
+    const handleClick = jest.fn();
+    const { getByText } = render(<Tab onClick={handleClick}>Favoriten</Tab>);
+    const tabElement = getByText("Favoriten");
+    fireEvent.click(tabElement);
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+});
